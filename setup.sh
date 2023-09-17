e753e2af44417dddf875db760506cfcbd9dfe3b2
Your Name <you@example.com>
2023-09-17 15:28:16 +0300

    working now

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
-# symlink .bashrc to of ~/.bashrc
-ln -s ./.bashrc ~/.bashrc
-# symlink init.lua to ~/.config/nvim 
-ln -s ./init.lua ~/.config/nvim/init.lua
-# symlink starship.toml to ~/.config 
-ln -s ./starship.toml ~/.config/starship.toml
+# link .bashrc to of ~/.bashrc
+ln ./.bashrc ~/.bashrc
+# link init.lua to ~/.config/nvim 
+ln ./init.lua ~/.config/nvim/init.lua
+# link starship.toml to ~/.config 
+ln ./starship.toml ~/.config/starship.toml
 # install packer
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 # install starship
@@ -13,4 +13,4 @@ mkdir fonts
 cd fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
 unzip ./JetBrainsMono.zip
-mv *.ttf /usr/local/share/fonts/
+mv *.ttf /usr/share/fonts/
