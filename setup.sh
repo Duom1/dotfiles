7cade3ad5bc5e281d432323ed9f99ec7dc7ecb6c
Your Name <you@example.com>
2023-09-18 09:12:18 +0300

    better setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,50 @@
-# link .bashrc to of ~/.bashrc
-ln ./.bashrc ~/.bashrc
-# link init.lua to ~/.config/nvim 
-ln ./init.lua ~/.config/nvim/init.lua
-# link starship.toml to ~/.config 
-ln ./starship.toml ~/.config/starship.toml
-# install packer
-git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
-# install starship
-curl -sS https://starship.rs/install.sh | sh
-# install nerdfonts
-mkdir fonts
-cd fonts
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
-unzip ./JetBrainsMono.zip
-mv *.ttf /usr/share/fonts/
+link_dotfiles() {
+  ln ./.bashrc ~/.bashrc
+  ln ./init.lua ~/.config/nvim/init.lua
+  ln ./starship.toml ~/.config/starship.toml
+}
+install_starship() {
+  curl -sS https://starship.rs/install.sh | sh
+}
+install_fonts() {
+  mkdir fonts
+  cd fonts
+  wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/JetBrainsMono.zip
+  unzip ./JetBrainsMono.zip
+  sudo mv *.ttf /usr/share/fonts/
+}
+clean() {
+  rm -rf ./fonts
+}
+
+# Main script
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 [all or link|starship|fonts|clean]"
+  exit 1
+fi
+
+for arg in "$@"; do
+  case "$arg" in
+    link)
+      link_dotfiles
+      ;;
+    starship)
+      install_starship
+      ;;
+    fonts)
+      install_fonts
+      ;;
+    clean)
+      clean
+      ;;
+    all)
+      link_dotfiles
+      install_starship
+      install_fonts
+      ;;
+    *)
+      echo "Unknown argument: $arg"
+      exit 1
+      ;;
+  esac
+done
